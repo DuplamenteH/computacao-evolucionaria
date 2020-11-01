de2f69243ee484edc9d6cf74983b9c2e8ddb82af
Carlos Matheus <38902593+DuplamenteH@users.noreply.github.com>
2020-11-01 18:29:36 -0300

    Capitulo 3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from numpy import exp, array, mgrid
 from pygeneric.populacao import Populacao
+from pygeneric.selecao.roleta import Roleta
 import matplotlib.pyplot as plt
 
 
@@ -24,12 +25,10 @@ def bin(x):
     enquanto a outra metade	representará a
     variável y
 """
-
-
 def xy( populacao ):
     colunas = populacao.shape[1]
     meio = colunas // 2
-    maiorbin = (2.0 ** meio) - 1.0
+    maiorbin = 2.0 ** meio - 1.0
     nmin = -3
     nmax = 3
     const = (nmax - nmin) / maiorbin
@@ -38,28 +37,35 @@ def xy( populacao ):
     return x, y
 
 
-def avalicao(populacao):
+def avaliacao(populacao):
     x, y = xy(populacao)
     tmp = func(x, y)
     return tmp
 
 
-############ Testes ##################
+############ Testes CAP 2##################
 
 genes_totais = 8
-tam_populacao = 5
-populacao = Populacao(avalicao, genes_totais, tam_populacao)
+tam_populacao = 100
+populacao =	Populacao(avaliacao,genes_totais,tam_populacao)
 populacao.gerar_pop()
 
-populacao.avaliar()
-x, y = xy(populacao.populacao)
+roleta=	Roleta(populacao)
 
-######## grafico ####
+pop	=roleta.selecao(10)
 
-fig = plt.figure(figsize=(100, 100))
-ax = fig.add_subplot(111,projection= "3d")
-X, Y = mgrid[-3:3:30j, -3:3:30j]
-Z = func(X, Y)
-ax.plot_wireframe(X, Y, Z)
-ax.scatter(x, y, func(x, y), s=100, c='red', marker='D')
+x,y =xy(pop)
+
+###################GRAFICO##############################
+fig	= plt.figure(figsize=(100,	100))
+
+ax=fig.add_subplot(111,	projection="3d")
+X,Y=mgrid[-3:3:30j,	-3:3:30j]
+Z=func(X,Y)
+ax.plot_wireframe(X,	Y,	Z)
+ax.scatter(x,	y,	func(x,	y),	s=50,	c='red',	marker='D')
 plt.show()
+
+
+
+
